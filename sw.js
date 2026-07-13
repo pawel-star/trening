f6be8b18e3d68baa64d3c7d5dd81b3c8b0d6d87b
pawel-star <pawel@saleshill.eu>
2026-07-13 13:49:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// prosty service worker: po pierwszym wejsciu aplikacja dziala w pelni offline
-const CACHE='trening-v1';
+// v2: strona pobierana najpierw z sieci (nowe wersje wchodza od razu), offline z cache
+const CACHE='trening-v2';
 const PLIKI=['./','./index.html','./manifest.webmanifest','./icon.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PLIKI)).then(()=>self.skipWaiting()));
@@ -8,5 +8,11 @@ self.addEventListener('activate',e=>{
   e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(r=>{
+      const kopia=r.clone(); caches.open(CACHE).then(c=>c.put('./index.html',kopia)); return r;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
   e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request)));
 });
